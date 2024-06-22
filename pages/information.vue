9d0b9905fabde1384b5a858e47f783836455d511
ZhannaKanal <kezhanna.03@mail.ru>
2024-06-22 20:20:20 +0500

    api of brands/information

--- pages/information.vue
+++ pages/information.vue
@@ -1,21 +1,16 @@
 <template>
-  <div>
+  <div v-if="infoList.id">
     <div
       class="max-w-[1280px] w-full mx-auto my-[80px] flex-col-reverse lg:flex-row flex justify-between gap-[200px]"
     >
       <div class="max-w-[636px] w-full">
-        <p class="font-extralight text-[30px] text-center lg:text-start">
-          Allegrini
+        <p class="font-extralight text-[30px]">
+          {{ infoList.title }}
         </p>
-        <p class="my-8">
-          Старинное хозяйство Allegrini ведет свою официальную винную историю с
-          1854 года. Настоящий подъем поместье пережило в 1960-х годах, когда
-          его хозяин, Джованни Аллегрини, стал активно развивать свое
-          винодельческое производство, постоянно внедряя нечто новое.
-        </p>
+        <p class="my-8" v-html="infoList.description"></p>
       </div>
       <div class="mx-auto">
-        <img src="~assets/images/image_85.png" alt="" />
+        <img :src="infoList.logo" alt="" />
       </div>
     </div>
     <div class="relative flex-[1_1_auto] max-w-[1280px] w-full mx-auto">
@@ -23,26 +18,24 @@
         class="absolute max-w-[300px] sm:max-w-[500px] md:max-w-[800px] w-full bg-[#CCAD7B] max-h-[480px] h-full right-6 top-0 z-[-1]"
       ></div>
       <div class="w-full py-6">
-        <img src="~/assets/images/image_86.png" draggable="false" alt="" />
+        <img :src="infoList.first_img" draggable="false" alt="" />
       </div>
     </div>
     <div class="bg-[#C49955] w-full">
-      <p class="text-center text-white py-6">
-        Управляющие винодельни: Франко Аллегрини и Марилиса Аллегрини <br />
-        Площадь территории составляет около 240 км² <br />
-        Главный офис – Венето , Италия
-      </p>
+      <p class="text-center text-white py-6" v-html="infoList.brand_info"></p>
     </div>
-    <div class="block lg:flex justify-between max-w-[1280px] w-full mx-auto my-12">
+    <div
+      class="block lg:flex justify-between max-w-[1280px] w-full mx-auto my-12"
+    >
       <div class="relative flex-[1_1_auto] max-w-[700px] w-full">
         <div class="relative">
           <div
             class="absolute max-w-[300px] sm:max-w-[500px] md:max-w-[800px] w-full bg-[#CCAD7B] max-h-[480px] h-full right-0 bottom-[-30px] z-[-1]"
           ></div>
-          <img src="~/assets/images/image_87.png" draggable="false" alt="" />
+          <img :src="infoList.sec_img" draggable="false" alt="" />
         </div>
         <div
-          class="absolute bottom-0 flex flex-row-reverse lg:flex-row gap-8 max-w-[560px] w-full h-[64px]"
+          class="my-[100px] flex flex-row-reverse lg:flex-row gap-8 max-w-[560px] w-full h-[64px]"
         >
           <div class="flex-[1_1_auto] bg-[#CCAD7B]"></div>
           <div class="w-[78px] bg-[#CCAD7B]"></div>
@@ -52,27 +45,12 @@
       </div>
       <div class="max-w-[580px] w-full mx-auto">
         <div class="px-4">
-          <p class="text-[30px] font-extralight uppercase">О компании</p>
-          <p class="py-4">
-            Им принадлежит 90 га собственных виноградников в коммунах Фумане,
-            Сант-Амброджо, Сан-Пьетро и Марано.
+          <p class="text-[30px] font-extralight uppercase">
+            {{ infoList.secondary_title }}
           </p>
-          <p>
-            Их главная отличительная особенность – это известняковые почвы,
-            повышающие в винограде концентрацию сахара и сохраняющие при этом
-            необходимую для создания вина кислотность.
-          </p>
-          <p class="py-4">
-            В основе успеха - серьезные исследования в области винификации
-            местных сортов винограда, а также ноу-хау и возрожденные методы,
-            такие как новая форма «рипассо». Все это, в конечном итоге,
-            формирует чистоту и стильное богатство букета, за которые ценят
-            продукцию Аллегрини как в Италии, так и за рубежом. Сегодня в
-            хозяйстве производят четыре основные марки красного вина: Recioto,
-            Valpolicella, Valpolicella Superiore и Amarone. В годы хорошего
-            урожая на свет выпускается около одного миллиона бутылок. Главным
-            сортом считается корвина веронезе.
-          </p>
+          <div class="py-4">
+            {{ infoList.main_text }}
+          </div>
         </div>
         <div class="flex flex-row-reverse flex-[1_1_auto] gap-4 min-h-[64px]">
           <div class="w-[18px] bg-[#CCAD7B]"></div>
@@ -82,25 +60,56 @@
           <div class="flex-[1_1_auto] bg-[#CCAD7B]"></div>
         </div>
         <div class="pt-4">
-            <img src="~assets/images/image_88.png" alt="">
+          <img :src="infoList.third_img" alt="" />
         </div>
       </div>
     </div>
     <div class="max-w-[1280px] w-full mx-auto">
-        <img src="~assets/images/image_89.png" alt="">
+      <img :src="infoList.main_img" alt="" />
     </div>
     <div
-        class="max-w-[1280px] w-full mx-auto my-8 flex gap-[100px] items-center"
+      class="max-w-[1280px] w-full mx-auto my-8 flex gap-[100px] items-center"
+    >
+      <p class="text-[30px] font-light">ТОВАРЫ</p>
+      <div
+        class="flex-[1_1_auto] bg-[white] min-h-[64px] h-full flex gap-7 order-[1] lg:order-[2]"
       >
-        <p class="text-[30px] font-light ">ТОВАРЫ</p>
-        <div class="flex-[1_1_auto] bg-[white] min-h-[64px] h-full flex gap-7 order-[1] lg:order-[2]">
-          <div class="w-[27px] bg-[#CCAD7B]"></div>
-          <div class="w-[54px] bg-[#CCAD7B]"></div>
-          <div class="w-[68px] bg-[#CCAD7B]"></div>
-          <div class="w-[95px] bg-[#CCAD7B]"></div>
-          <div class="flex-[1_1_auto] bg-[#CCAD7B]"></div>
-        </div>
+        <div class="w-[27px] bg-[#CCAD7B]"></div>
+        <div class="w-[54px] bg-[#CCAD7B]"></div>
+        <div class="w-[68px] bg-[#CCAD7B]"></div>
+        <div class="w-[95px] bg-[#CCAD7B]"></div>
+        <div class="flex-[1_1_auto] bg-[#CCAD7B]"></div>
       </div>
+    </div>
+    <div class="my-12">
+      <img src="https://brut.kz/storage/43847/7663.jpg" alt="">
+      <!-- <v-sheet class="mx-auto"  max-width="1440">
+        <v-slide-group
+          v-model="model"
+          class="pa-4"
+          selected-class="bg-success"
+          
+        >
+          <v-slide-group-item
+            v-for="product in infoList.products"
+            :key="product.id"
+            v-slot="{ infoList, toggle, selectedClass }"
+          >
+            <v-card
+              :class="['ma-1', selectedClass]"
+              height="450"
+              width="319"
+              @click="toggle"
+            >
+              <div class="d-flex fill-height align-center justify-center">
+                <img :src="'https://brut.kz'+product.image" alt="">
+                <p>{{product.name}}</p>
+              </div>
+            </v-card>
+          </v-slide-group-item>
+        </v-slide-group>
+      </v-sheet> -->
+    </div>
   </div>
 </template>
 
@@ -112,8 +121,26 @@ export default {
       layout: "contacts",
       middleware: "",
     });
-
-    return {};
+    const infoList = ref({});
+    // onMounted(() =>{
+    //   getInfo();
+    // });
+    onMounted(async () => {
+      await getInfo();
+    });
+    const getInfo = async () => {
+      await axios
+        .get("https://pma.kws.kz/api/brands/358")
+        .then(function (response) {
+          infoList.value = response.data;
+          console.log(infoList.value);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .finally(function () {});
+    };
+    return { infoList };
   },
 };
 </script>
---
+++ pages/products/[id].vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        const { id } = useRoute().params
+
+        return {id}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
